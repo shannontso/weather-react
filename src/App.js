545e15dff18df9b620b90a05690560ed25086bbe
shannontso <shannontan98@yahoo.com>
2021-07-06 13:24:09 +0200

    added GitHub link

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ export default function App() {
       <div className="container">
         <Weather />
       </div>
+      <footer>
+        This project was coded by Shannon Tan and is{" "}
+        <a
+          href="https://github.com/shannontso"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
