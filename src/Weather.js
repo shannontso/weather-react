8c6d5180ce6e0c70a1f2c8cfb9e949521b85dd4f
shannontso <shannontan98@yahoo.com>
2021-07-14 12:10:34 +0200

    updated API url

--- src/Weather.js
+++ src/Weather.js
@@ -23,7 +23,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "159a9f8ff294f264def02ae4cac4278a";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
